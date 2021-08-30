3bc7285142ef58915f3b73e03499ecc23cbfa4f0
hr955 <nfzoze01@gmail.com>
2021-08-30 11:53:26 +0900

    Scanner를 이용하여 6개의 숫자 입력받기

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,9 +1,23 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 	public static void main(String[] args) {
-		//github upload
+		// github upload
+		
+		Scanner scanner = new Scanner(System.in);
 
+		// 입력한 6개의 숫자를 저장할 배열
+		int[] inputNumbers = new int[6];
+
+		System.out.print("6개의 숫자 입력 : ");
+		// 사용자에게 6개의 숫자를 입력받음
+		for (int i = 0; i < inputNumbers.length; i++) {
+			inputNumbers[i] = scanner.nextInt();
+		}
+		
+		scanner.close();
 	}
 
 }
